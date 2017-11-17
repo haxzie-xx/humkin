69d8d10133de66ae99ffa8b53f725ee32eda1bd2
Musthaq Ahamad <musthu.gm@gmail.com>
2017-11-17 22:58:58 +0530

    Campaign completed

--- src/Pages/DashPages/Camps/Campaign.js
+++ src/Pages/DashPages/Camps/Campaign.js
@@ -1,35 +1,60 @@
-import React, {Component} from 'react'
-import {Row, Col, Input, Button} from 'react-materialize'
+import React, {Component} from 'react';
+import {Button} from 'react-materialize';
+import CampList from './CampList';
+import CreateCamp from './CreateCamp';
+import CampEdit from './CampEdit';
+import Auth from '../../../auth';
+let auth = new Auth();
 
 class Campaign extends Component{
 
+    constructor(props){
+        super(props);
+        this.state = {
+            screenId : 0,
+            camp_id : ''
+        }
+    }
+    getCurrentView(){
+        if(this.state.screenId === 1)
+            return <CreateCamp closeCallBack={ this.closeCallBack }/>;
+        else if(this.state.screenId === 2)
+            return <CampEdit camp_id={this.state.camp_id} closeCallBack={ this.closeCallBack } />
+
+        return <CampList editCallBack={ this.editCallBack } closeCallBack={ this.closeCallBack } />;
+    }
+
+    getButtonIcon(){
+        if(this.state.screenId === 1 || this.state.screenId === 2)
+                    return "close";
+        else return "add";
+    }
+
+    handleClick = () => {
+        let sid = this.state.screenId === 0 ? 1 : 0 ;
+        this.setState((prevState, props) => {
+          return { screenId: sid }     
+        });
+      }
+
+    editCallBack = (cid) =>{
+        this.setState((prevState, props) => {
+            return { screenId: 2, camp_id: cid}
+        });
+    }
+
+    closeCallBack = () => {
+        this.setState({ screenId : 0 });
+    }
+    
+
     render(){
         return(
-            <Row>
-            
-                <div className="basic_card m10top row pad20 card-1">
-                    <Row>
-                        <p className="card_title">Create Campaign</p>
-                        <hr className="_small_line" />  
-                    </Row>
-                    <Input s={12} m={6} type="text" label="Camp Name" validate/>
-                    <Input s={12} m={6} type="tel" label="Contact Number" validate/>
-                    <Input s={12} m={6} type="text" placeholder="" label="Start Date" onChange={(e, value) => {}}  />
-                    <Input s={12} m={6} type="text" placeholder="" label="End Date" onChange={(e, value) => {}} />
-                    <Input s={12} m={6} type="text" label="Location/ City" validate/>
-                    <Input s={12} m={6} type="text" label="Address Line 1" validate/>
-                    <Input s={12} m={6} type="text" label="Address Line 2" validate/>
-                    <Input s={12} m={6} type="text" label="Caption" validate/>
-                    < br/>
-                    <Col s={12}>
-                    <Col s={6}/>
-                    <div className="col s6 m0 pad10 right">
-                        <Button className="m10 waves-effect waves-light red accent-2">Cancel</Button>
-                        <Button className="m10 waves-effect waves-light red accent-2">Create Campaign</Button>
-                    </div>
-                    </Col>
-                </div>
-             </Row>
+            <div className="basic_card pad20 card-2 m20top">
+                { this.getCurrentView() }
+                <Button floating className='red' large style={{bottom: '45px', right: '24px', position: 'fixed'}} onClick={ this.handleClick }><i className="material-icons">{ this.getButtonIcon() }</i></Button>
+
+            </div>
         )
     }
 }
